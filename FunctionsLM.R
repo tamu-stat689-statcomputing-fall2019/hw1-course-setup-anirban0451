9cea151ba0d15aab1d16adc62c97c5732e19a46b
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-08-29 22:22:05 -0500

    This commit will generate data Y when applied

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -5,7 +5,7 @@
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
-  
+  Y=as.vector(X%*%(as.matrix(beta))+as.matrix(rnorm(n=nrow(X),mean=0,sd=sigma)))
   # Return Y
   return(Y)
 }
